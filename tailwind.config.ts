31eb762abb6eb01929f40b93286a896170f21f39
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-11-03 12:15:41 +0530

    Initial tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -13,8 +12,23 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens: {
+        xs: '400px',
+        '3xl': '1680px',
+        '4xl': '2200px',
+      },
+      maxWidth: {
+        '10xl': '1512px',
+      },
+      borderRadius: {
+        '5xl': '40px',
+      },
+      colors: {
+        blue: {
+          100: '#0d0c22',
+        }
+      }
     },
   },
   plugins: [],
 }
-export default config
