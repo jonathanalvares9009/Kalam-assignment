25e0875116d0972fc95b7a725b6b5d69220adf83
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-11-03 19:29:56 +0530

    Created samples component

--- components/Samples.tsx
+++ components/Samples.tsx
@@ -1,6 +1,22 @@
+import { SAMPLES } from "@/constants"
+import Sample from "./Sample"
+
 const Samples = () => {
   return (
-    <div>Samples</div>
+    <section className="flex flex-col gap-4 px-4">
+      <div className="flex justify-between">
+        <p className="font-bold">More by Nailul Izah</p>
+        <p className="font-light">View profile</p>
+      </div>
+
+      <div className="flex flex-col gap-8">
+        {
+          SAMPLES.map((sample, index) => (
+            <Sample key={index} alt={sample.key} src={sample.src} />
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
