ae5f89fdc61d5e8f737794e9851b915387e6fedc
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-11-03 12:18:10 +0530

    Created constants file

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+export const NAV_LINKS = [
+    { href: 'https://dribbble.com/hiring', key: 'find_talent', label: 'Find talent' },
+    { href: 'https://dribbble.com/shots/popular', key: 'inspiration', label: 'Inspiration' },
+    { href: 'https://dribbble.com/learn', key: 'learn_design', label: 'Learn design' },
+    { href: 'https://dribbble.com/jobs', key: 'jobs', label: 'Jobs' },
+    { href: 'https://dribbble.com/pro', key: 'go_pro', label: 'Go Pro' },
+];
