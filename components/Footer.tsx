f04072f8718c64c09f29506a515a45bae96343d1
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-11-03 21:01:42 +0530

    Created mobile version of footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import { FOOTER_LINKS, LINKS, SOCIALS } from "@/constants"
+import Image from "next/image"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className='flex flex-col gap-20 pt-48 pb-6 px-8 items-center'>
+      <div className="flex flex-col gap-4">
+        <div className='flex flex-col items-center'>
+          <Image src="/dribble-logo.svg" width={76} height={30} alt="Dribble Logo" />
+        </div>
+
+        <ul className="flex flex-wrap justify-center gap-y-1 gap-x-4">
+          {LINKS.map((link, index) => (
+            <li key={index}>
+              <a href={link.href} className="font-medium">
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <ul className="flex flex-wrap justify-center gap-y-1 gap-x-4">
+          {SOCIALS.map((social, index) => (
+            <li key={index}>
+              <a href={social.href} className="font-medium">
+                <Image src={social.icon} width={20} height={20} alt={social.alt} />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+
+      <ul className="flex flex-wrap-reverse justify-center gap-y-1 gap-x-4">
+        {FOOTER_LINKS.map((link, index) => (
+          <li key={index}>
+            {
+              link.href ? 
+                <a href={link.href} className="font-light">
+                  {link.label}
+                </a> : 
+                <p className="font-light">
+                  {link.label}
+                </p>
+            }
+          </li>
+        ))}
+      </ul>
+    </section>
   )
 }
 
--- constants/index.ts
+++ constants/index.ts
@@ -20,3 +20,34 @@ export const SAMPLES = [
     { src: "/sample3.png", alt: "Sample Three", key: "sample3" },
     { src: "/sample4.png", alt: "Sample Four", key: "sample4" },
 ]
+
+export const LINKS = [
+    { href: "https://dribbble.com/for-designers", label: "For designers", key: "for_designers" },
+    { href: "https://dribbble.com/hiring", label: "Hire talent", key: "hire_talent" },
+    { href: "https://dribbble.com/shots/popular", label: "Inspiration", key: "inspiration" },
+    { href: "https://dribbble.com/advertise", label: "Advertising", key: "advertising" },
+    { href: "https://dribbble.com/stories", label: "Blog", key: "blog" },
+    { href: "https://dribbble.com/about", label: "About", key: "about" },
+    { href: "https://dribbble.com/careers", label: "Careers", key: "careers" },
+    { href: "https://dribbble.com/contact", label: "Support", key: "support" },
+]
+
+export const SOCIALS = [
+    { icon: "/twitter.svg", alt: "twitter", key: "twitter", href: "http://twitter.com/dribbble" },
+    { icon: "/facebook.svg", alt: "facebook", key: "facebook", href: "http://facebook.com/dribbble" },
+    { icon: "/instagram.svg", alt: "instagram", key: "instagram", href: "http://instagram.com/dribbble" },
+    { icon: "/pinterest.svg", alt: "pinterest", key: "pinterest", href: "http://www.pinterest.com/dribbble" },
+]
+
+export const FOOTER_LINKS = [
+    { href: "https://dribbble.com/directories/jobs", label: "Jobs", key: "jobs" },
+    { href: "https://dribbble.com/directories/designers", label: "Designers", key: "designers" },
+    { href: "https://dribbble.com/directories/freelance-designers", label: "Freelancers", key: "freelancers" },
+    { href: "https://dribbble.com/tags", label: "Tags", key: "tags" },
+    { href: "https://dribbble.com/places", label: "Places", key: "places" },
+    { href: "https://dribbble.com/resources", label: "Resources", key: "resources" },
+    { label: "© 2023 Dribbble", key: "dribble" },
+    { href: "https://dribbble.com/terms", label: "Terms", key: "terms" },
+    { href: "https://dribbble.com/privacy", label: "Privacy", key: "privacy" },
+    { href: "https://dribbble.com/cookie-policy", label: "Cookies", key: "cookies" },
+]
