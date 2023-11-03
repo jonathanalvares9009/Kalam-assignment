c34d80d287ceafe3811eb3d6a7d131d263dea673
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-11-03 19:12:14 +0530

    Created the mobile version of the profile component

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,6 +1,41 @@
+import { ACTIONS } from "@/constants"
+import Image from "next/image"
+
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <section className="flex flex-col gap-8 px-4">
+      <p>Hello folks!</p>
+      <p>Wemsc is a music streaming platform to accompany you every time.</p>
+      <p>What do you think? Give me feedback, thank you!</p>
+      <div>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+      </div>
+      <div>
+        <p>Connect with me</p>
+        <p>nailulizah13@gmail.com | <span className="border-pink-500 border-b-2 py-0.5">Instagram</span> | <span className="border-pink-500 border-b-2 py-0.5">LinkedIn</span></p>
+      </div>
+
+      <div className="flex flex-col py-12 gap-8">
+        <div className="flex gap-2 justify-center">
+          {ACTIONS.map((action, index) => (
+            <div key={index} className="p-2 rounded-full border-2 border-gray-50">
+              <Image src={action.src} width={16} height={16} alt={action.key} key={action.key} />
+            </div>
+          ))}
+        </div>
+
+        <div className="flex flex-col gap-2 items-center">
+          <div className="flex gap-4 w-full items-center">
+            <span className="border border-gray-50 w-2/5"></span>
+            <Image src="/profile.png" width={20} height={20} alt="Profile" className="rounded-full h-16 w-16" />
+            <span className="border border-gray-50 w-2/5"></span>
+          </div>
+          <p>Nailul Izah</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
