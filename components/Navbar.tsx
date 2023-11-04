c2aebea83e9d6067619d61beeb62609e135a59c4
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-11-04 12:07:52 +0530

    Created the mobile menu version

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,27 +3,36 @@ import { ACTIONS, NAV_LINKS } from "@/constants"
 import Image from "next/image"
 import Link from "next/link"
 import Button from "./Button"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  // const navbarRef = useRef<HTMLElement>(null);
+  const [isMenuOpen, setMenuIsOpen] = useState(false);
 
-  const handleClick = () => {
-    setIsOpen(!isOpen);
-  };
+  // useEffect(() => {
+  //   if (!navbarRef.current) return;
+  //   const navbarHeight = navbarRef.current.clientHeight;
+  //   const ulElement = document.getElementById('menu-links');
+
+  //   if (isMenuOpen && ulElement) {
+  //     ulElement.style.top = `${navbarHeight}px`;
+  //   } else if (!isMenuOpen && ulElement) {
+  //     ulElement.style.top = '0';
+  //   }
+  // }, [isMenuOpen]);
   
   return (
     <nav className="flex justify-between relative z-1 py-5 px-4 h-24 selection:bg-yellow-50">
       <div className="flex items-center gap-4">
-        <div className={`flex flex-col h-4 w-5 justify-between normal bg-transparent lg:hidden ${isOpen ? 'x-open' : ''}`} onClick={() => setIsOpen(!isOpen)}>
+        <div className={`flex flex-col h-4 w-5 justify-between normal bg-transparent lg:hidden ${isMenuOpen ? 'x-open' : ''}`} onClick={() => setMenuIsOpen(!isMenuOpen)}>
           <span className="w-full h-0.5 bg-blue-100"></span>
           <span className="w-full h-0.5 bg-blue-100"></span>
           <span className="w-2/3 h-0.5 bg-blue-100"></span>
         </div>
 
 
-        <Image src="/dribble-logo.svg" width={76} height={30} alt="Dribble Logo" className="block select-none xl:hidden" />
+        <Image src="/dribble-logo.svg" width={76} height={30} alt="Dribble Logo" className="block select-none xl:hidden lg:w-28" />
         <ul className="hidden h-full gap-6 lg:flex">
           {NAV_LINKS.map((link) => (
             <Link href={link.href} key={link.key} className="regular-16 text-blue-100 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
@@ -33,7 +42,7 @@ const Navbar = () => {
         </ul>
       </div>
 
-      <Image src="/dribble-logo.svg" width={76} height={30} alt="Dribble Logo" className="hidden select-none xl:block" />
+      <Image src="/dribble-logo.svg" width={124} height={30} alt="Dribble Logo" className="hidden select-none xl:block" />
 
       <div className="flex">
         <div className="items-center border border-gray-300 bg-gray-300 rounded-full px-4 hidden h-full gap-6 xl:flex">
@@ -52,7 +61,27 @@ const Navbar = () => {
               <Image src={action.src} width={16} height={16} alt={action.key} key={action.key} />
             </div>
           ))}
-        </div>
+      </div>
+
+      {isMenuOpen && (
+            <div id="menu-links" className="fixed w-full top-24 left-0 opacity-1 visible border-t-2 border-blue-100 bg-white lg:hidden">
+                <ul className="flex flex-col gap-2 justify-center px-6 py-6 bg-slate">
+                  {NAV_LINKS.map((link) => (
+                      <li key={link.key} className="mx-2">
+                          <Link href={link.href} className="bold-16 text-gray-500 hover:text-gray-800">
+                              {link.label}
+                          </Link>
+                      </li>
+                  ))}
+                  <hr />
+                  <li className="mx-2">
+                      <Link href={""} className="bold-16 text-gray-500 hover:text-gray-800">
+                          Log in
+                      </Link>
+                  </li>
+                </ul>
+            </div>
+        )}
     </nav>
   )
 }
