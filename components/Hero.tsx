aadcfc0fd935cf96b7286af5c5e2d5902b581609
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-11-08 11:48:09 +0530

    Corrected the colors in the hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
   return (
     <section className="flex flex-col gap-10 py-8 lg:items-center pb-16 selection:bg-yellow-50 sm:items-center">
       <div className="flex flex-col gap-2 px-4 lg:hidden">
-        <h1 className="text-blue-100 font text-xl font-medium">
+        <h1 className="text-blue-100 text-2xl font-semibold">
           Wemsc - Music Streaming Desktop App (Dark Mode)
         </h1>
         <div className="flex gap-4">
@@ -23,9 +23,9 @@ const Hero = () => {
             alt="Profile"
             className="hover:animate-pulse rounded-full h-12 w-12 select-none"
           />
-          <div className="flex flex-col h-full text-sm justify-items-center gap-2">
+          <div className="flex flex-col h-full text-sm justify-items-center gap-2 text-blue-100 font-semibold">
             <p>Nailul Izah</p>
-            <p>Follow</p>
+            <p className="text-[#6e6d7a] font-medium text-xs">Follow</p>
           </div>
         </div>
       </div>
@@ -41,16 +41,16 @@ const Hero = () => {
           />
         </ProfileTooltip>
         <div className="flex flex-col gap-2">
-          <h1 className="text-blue-100 font text-xl font-medium">
+          <h1 className="text-blue-100 text-2xl font-semibold">
             Wemsc - Music Streaming Desktop App (Dark Mode)
           </h1>
-          <div className="flex h-full text-sm justify-items-center items-center gap-2">
+          <div className="flex h-full text-sm justify-items-center items-center gap-2 text-blue-100 font-semibold">
             <ProfileTooltip needsFollowButton={true} placement="bottom">
               <p>Nailul Izah</p>
             </ProfileTooltip>
             <span className="inline-block w-1 h-1 border-2 border-black rounded-full"></span>
             <Tooltip content="Follow Nailul Izah" placement="top">
-              <p>Follow</p>
+              <p className="text-[#6e6d7a] font-medium text-xs">Follow</p>
             </Tooltip>
           </div>
         </div>
