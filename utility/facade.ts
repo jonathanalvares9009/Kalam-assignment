3a5ea3b7e60c65ced31baacadb1b53450e739d33
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-11-08 12:17:06 +0530

    Changed the animations to a lighter theme

--- utility/facade.ts
+++ utility/facade.ts
@@ -2,14 +2,14 @@ export const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
+      <stop stop-color="#f7f7f7" offset="20%" />
+      <stop stop-color="#eaeaea" offset="50%" />
+      <stop stop-color="#f7f7f7" offset="70%" />
     </linearGradient>
   </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
+  <rect width="${w}" height="${h}" fill="#fff" />
   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite" />
 </svg>`;
 
 export const toBase64 = (str: string) =>
